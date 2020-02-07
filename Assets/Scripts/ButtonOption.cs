21b85d975820d846da30caf8e7af5170dcefb5f1
Tóth Bálint Benedek <toth.balint.benedek@gmail.com>
2020-02-07 17:09:27 +0100

    first version

---
+++ Assets/Scripts/ButtonOption.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonOption : MonoBehaviour
+{
+
+    public GameObject AboutPanel;
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadAboutScreen()
+    {
+        AboutPanel.SetActive(true);
+    }
+
+    public void CloseAboutScreen()
+    {
+        AboutPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
